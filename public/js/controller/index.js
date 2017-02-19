e601196d5303265ab44cdac17568ce4e3252857e
deivansh <ardhanghani@gmail.com>
2017-02-19 17:35:33 +0530

    final commit

---
+++ public/js/controller/index.js
@@ -0,0 +1,14 @@
+'use strict'
+var app = require('angular').module('app2');
+
+app.controller('MovieController', require('./moviecontroller'));
+app.controller('TheatreController', require('./theatrecontroller'));
+app.controller('HomeController', require('./homecontroller'));
+app.controller('BookingController', require('./bookingcontroller'));
+app.controller('PaymentController', require('./paymentcontroller'));
+app.controller('MappingController', require('./mappingcontroller'));
+app.controller('ConfirmpageController', require('./confirmpagecontroller'));
+app.controller('LoginController', require('./loginController'));
+app.controller('LogoutController', require('./logoutController'));
+app.controller('RegisterController', require('./registerController'));
+app.controller('IndexrController', require('./indexcontroller'));
